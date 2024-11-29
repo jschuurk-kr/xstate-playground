2526e703558460d81e6a33361c5bc1573b34a55d
Joep <145749778+jschuurk-kr@users.noreply.github.com>
2024-11-29 20:02:03 +0100

    adds first experiment

---
+++ README.md
@@ -0,0 +1,13 @@
+# xstate playground
+
+## references
+https://dev.to/ryanroselloog/model-based-testing-using-playwright-and-xstate-what-ive-learnt-so-far-4180
+
+https://stately.ai/viz
+https://gist.github.com/ryanrosello-og/671aab64b3c0765d545c0d71813bafdc
+
+https://stately.ai/docs/testing
+
+https://graph-docs.vercel.app/model-based-testing/playwright
+
+https://stately.ai/docs/migration
---
+++ tests/example.spec.ts
@@ -0,0 +1,51 @@
+import { test, expect } from '@playwright/test';
+import { createMachine } from "xstate";
+import { createTestModel, createTestMachine } from "@xstate/test";
+
+
+const machine = createTestMachine({
+  id: 'documentSearch',
+  initial: 'onHomePage',
+  states: {
+    onHomePage: {
+      on: {
+        CLICK_GET_STARTED: 'introPage',
+        CLICK_DOCS: 'apiPage',
+      },
+    },
+    introPage: {},
+    apiPage: {}
+  },
+});
+
+test.describe("My app", () => {
+  createTestModel(machine)
+    .getSimplePaths()
+    .forEach((path) => {
+      test(path.description, async ({ page }) => {
+        await path.test({
+          states: {
+            onHomePage: async () => {
+              await page.goto('https://playwright.dev/');
+              await expect(page).toHaveTitle(/Playwright/);
+            },
+            introPage: async () => {
+              await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+            },
+            apiPage: async () => {
+              await expect(page.getByRole('heading', { name: 'Playwright Library' })).toBeVisible();
+            }
+          },
+          events: {
+            CLICK_GET_STARTED: async () => {
+              page.getByRole("link", {name: "Get started"}).click()
+            },
+            CLICK_DOCS: async () => {
+              page.getByRole("link", {name: "API"}).click()
+            }
+          },
+          
+        });
+      });
+    });
+});
